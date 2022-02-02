27e17930cc0ad4fc3778fd56fb5cb3c6962a1d65
nikulin-me <itdetectiv@gmail.com>
2022-02-02 20:56:23 +0400

    Tested MessageErrorCreatorTest

--- src/test/java/server/library/service/MessageErrorCreatorTest.java
+++ src/test/java/server/library/service/MessageErrorCreatorTest.java
@@ -1,12 +1,32 @@
 package server.library.service;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MessageErrorCreatorTest {
 
     @Test
-    void makeErrorMessage() {
+    void makeErrorMessage() throws NoSuchMethodException {
+        String request = "getBooksByParams";
+        List<Optional<Object>> values = List.of(Optional.of("Bible"), Optional.of(Set.of("God")));
+        String expected = "{name=Bible, authors=[God]}";
+
+        try (MockedStatic<MessageErrorCreator> mockedStatic = Mockito.mockStatic(MessageErrorCreator.class)) {
+
+            mockedStatic.when(() -> MessageErrorCreator.makeErrorMessage(request, values)).thenReturn("{name=Bible, authors=[God]}");
+            String result = MessageErrorCreator.makeErrorMessage(request, values);
+
+            assertEquals(expected, result);
+            mockedStatic.verify(() -> BookServiceParameterInterceptor.getParametersFromMethod(request));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
