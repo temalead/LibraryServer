bd8c01fb47cf43c7bb75de5171ab3337f88a9b4f
nikulin-me <itdetectiv@gmail.com>
2022-02-08 11:05:49 +0400

    Deleted LibraryExistByAddressException useless methods

--- src/main/java/server/library/exception/LibraryExistByAddressException.java
+++ src/main/java/server/library/exception/LibraryExistByAddressException.java
@@ -8,12 +8,4 @@ public class LibraryExistByAddressException extends RuntimeException {
     public LibraryExistByAddressException(String message) {
         super("Library with "+message+" already exists!");
     }
-
-    public LibraryExistByAddressException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LibraryExistByAddressException(Throwable cause) {
-        super(cause);
-    }
 }
--- src/test/java/server/library/controller/SpringTestConfig.java
+++ src/test/java/server/library/controller/SpringTestConfig.java
@@ -7,8 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 import server.library.domain.Book;
 
 import javax.persistence.EntityManager;
@@ -19,7 +27,24 @@ import java.util.List;
 @Sql(value = "/test/migration/AFTER_TESTS.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 public abstract class SpringTestConfig {
+
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer=new PostgreSQLContainer("postgres:14.1")
+            .withDatabaseName("integreation-tests-db")
+            .withUsername("sa")
+            .withPassword("sa");
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
     @Autowired
     protected MockMvc mvc;
